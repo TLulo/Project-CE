e75d1ebd4bf47fda7e03a449ee4dbb40fc05988b
TLulo <luciano.tula@mi.unc.edu.ar>
2026-09-08 16:24:06 -0300

    Se Crea la clase Player, de la cual se heredaran otras subclases para juegos mas especificos

---
+++ src/Player/Player.java
@@ -0,0 +1,55 @@
+package Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Card.Card;
+import SetCard.Hand;
+
+public class Player {
+    protected static int nextId = 0;
+    protected int playerId;
+    protected Hand hand;
+    protected List<Card> playerTable;
+    protected String name;
+
+    public Player(){
+        playerTable = new ArrayList<>();
+        playerId = nextId ++;
+    }
+
+    public void setPlayerHand(Hand newHand){
+        hand = newHand;
+    }
+
+    public Hand getPlayerHand(){
+        return hand;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPlayerId(){
+        return playerId;
+    }
+
+    public void putCardinTable(int cardId){
+        playerTable.add(hand.takeSearchCard(cardId));
+    }
+
+    public Card removeToTable(int CardId){
+        for (int i = 0; i < playerTable.size(); i++ ) {
+            if (playerTable.get(i).matchesId(CardId)){
+                return playerTable.remove(i);
+            }
+        }
+        System.out.println("Warning (TakeSearchCard): id not in Table");
+        return null;
+    }
+
+}
