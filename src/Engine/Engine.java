506c6ca7c918527b70ebce5f8358170f48d8206f
TLulo <luciano.tula@mi.unc.edu.ar>
2026-09-11 15:29:01 -0300

    Se anade la clase Engine, falta una revision

--- src/Engine/Engine.java
+++ src/Engine/Engine.java
@@ -1,5 +1,6 @@
 package Engine;
 
+import Player.Player;
 import Table.Table;
 
 public class Engine {
@@ -8,6 +9,33 @@ public class Engine {
 
     public Engine(Table table){
         this.table = table;
-        this.currentTurn = 0;
+        currentTurn = 0;
+    }
+
+    public Player getCurrentPlayer(){
+        if (!table.getPlayers().isEmpty()){
+            currentTurn = (currentTurn + 1) % table.getPlayers().size();
+            return table.getPlayers().get(currentTurn);
+        }
+        System.out.println("Warning (getCurrentPlayer): no players at Table");
+        return null;
+    }
+
+    public Table getTable(){
+        return table;
+    }
+    
+    public Player nextTurn(){
+        if (!table.getPlayers().isEmpty()){
+            currentTurn = (currentTurn + 1) % table.getPlayers().size();
+            return getCurrentPlayer();
+        }
+        System.out.println("Warning (nextTurn): no players at Table");
+        return null;
+    }
+
+    public void setGame(int handSize){
+        table.setGame(handSize);
+        currentTurn = 0;
     }
 }
--- src/Table/Table.java
+++ src/Table/Table.java
@@ -4,10 +4,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Player.Player;
+import SetCard.Deck;
+import SetCard.Hand;
 
 public abstract class Table {
     protected  List<Player> players = new ArrayList<>();
-    
+    protected Deck mainDeck;
+
+    /*
+     * SubClases should be implemented setGame
+     * with necesary Decks and other stuffs 
+     */
+    public abstract void setGame(int handSize);
+
+
+    /**********************************
+     * 
+     * MainDeck Methods
+     */
+    public void setMainDeck(Deck newDeck){
+        mainDeck = newDeck;
+    }
+
+    public Deck getMainDeck(){
+        return mainDeck;
+    }
+
+    public void dealHands(int handSize){
+        for (Player player : players) {
+            player.setPlayerHand(new Hand(mainDeck.deal(handSize),handSize));
+        }
+    }
+
+    public void shuffleMainDeck(){
+        mainDeck.shuffle();
+    }
+
+    /**************************************** */
+    //Player Methods
     public void addPlayer(Player newPlayer){
         players.add(newPlayer);
     }
